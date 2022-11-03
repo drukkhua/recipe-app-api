dc6f99da3e96bd03a7c943f111d329050a866de7
Aleksieiev-Valentyn <director@druk.kh.ua>
2022-11-03 20:33:14 +0200

    PostgreSQL added

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+'''
+Calculator functions
+'''
+
+
+def add(x, y):
+    """add x to y and return result"""
+    return x + y
+
+
+def subtract(x, y):
+    """subtract x from x return result"""
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample test
+"""
+from django.test import SimpleTestCase
+
+from .calc import add, subtract
+
+
+class CalcTest(SimpleTestCase):
+    '''Test the calc module.'''
+
+    def test_add_numbers(self):
+        """Test adding number together."""
+        res = add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_substact_numbers(self):
+        """Test subtracting numbers."""
+        res = subtract(10, 15)
+
+        self.assertEqual(res, 5)
